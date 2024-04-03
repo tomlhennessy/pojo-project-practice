b5d7fa3fb03368fbd26a4989b4be52a7de5b620b
Tom Hennessy <tomlhennessy@outlook.com>
2024-04-03 13:05:32 +0100

    problems 1, 2, 3 complete

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -1,5 +1,5 @@
 /***********************************************************************
-Write a function called `keysInObject(obj)` that takes in an object and returns 
+Write a function called `keysInObject(obj)` that takes in an object and returns
 an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
@@ -11,9 +11,26 @@ let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
 keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
-
+// function to extract keys from an object using a for...in loop
 function keysInObject(obj) {
-  // Your code here
+  // initialise empty array to store keys
+  let keysArray = [];
+  // iterate over each property in the object
+  for (let key in obj) {
+    // check if the property is an own property of the object (not inherited)
+    if (obj.hasOwnProperty(key)) {
+      // push the key into the keysArray
+      keysArray.push(key);
+    }
+  }
+  // return the array containing keys
+  return keysArray;
+}
+
+// function to extract keys from an object using Object.keys()
+function keysInObject2(obj) {
+  // return an array containing the keys extracted using Object.keys()
+  return Object.keys(obj);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
--- problems/02-values-in-object.js
+++ problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -13,10 +13,29 @@ let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
 valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
 valuesInObject(foods); // => ["tart", "sour", "sweet"]
 ***********************************************************************/
-
+// function to extract values from an object using for...in loop
 function valuesInObject(obj) {
-  // Your code here
+  // initialise an empty array to store values
+  let valuesArray = [];
+  // iterate over each property in the object
+  for (let key in obj) {
+    // check if the property is an own property of the object (not inherited)
+    if (obj.hasOwnProperty(key)) {
+      // push the value associated with the current key into the valuesArray
+      valuesArray.push(obj[key]);
+    }
+  }
+  // return array containing values
+  return valuesArray;
 }
+
+// function to extract values from an object using Object.values()
+function valuesInObject(obj) {
+  // return an array containing the values extracted using Object.values()
+  return Object.values(obj);
+}
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuesInObject;
